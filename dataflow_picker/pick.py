61fa8cecde7593a7270b18d40b631f1509fdfead
Inderaj <isehmi99@gmail.com>
2023-03-17 23:49:01 +1000

    Update pick.py
    added formatting for agencyId and version

--- dataflow_picker/pick.py
+++ dataflow_picker/pick.py
@@ -7,6 +7,10 @@ find = "<structure:Dataflow id="
 fp = open(file, 'r')
 line_test = 'Line: <structure:Dataflow id="LC_CM_15" agencyID="ABS" version="1.0.0" isFinal="true">'
 save_data = []
+agencyId = []
+version = []
+
+dataflow = pd.DataFrame(columns=['agencyId', 'dataflowId', 'version'])
 
 def open_file(file_name, word):
     with open(file_name, 'r') as file:
@@ -27,14 +31,23 @@ def open_line(file_name, word):
 
 
 def formang_line(line):
-    start = "id="
-    end = "agencyID"
-    index_start = line.find(start)
-    index_end = line.find(end)
-    # print(index_start," --- ", index_end)
-    temp = line[index_start+4:index_end-2]
-    # print(temp)
-    save_data.append(temp)
+    start_id = "id="
+    end_id = "agencyID"
+
+    start_agencyId = 'agencyID='
+    end_agencyId = 'version'
+
+    start_version = "version="
+    end_version = 'isFinal'
+
+    temp_id = line[line.find(start_id)+4:line.find(end_id)-2]
+    temp_agencyID = line[line.find(start_agencyId)+10:line.find(end_agencyId)-2]
+    temp_version = line[line.find(start_version)+9:line.find(end_version)-2]
+
+    save_data.append(temp_id)
+    agencyId.append(temp_agencyID)
+    version.append(temp_version)
+    
 
 
 
@@ -43,6 +56,20 @@ def formang_line(line):
 open_line(file, find)
 # formang_line(line_test)
 
-df = pd.DataFrame(save_data, columns=["id"])
+# df = pd.DataFrame(save_data, columns=["id"])
+df = pd.DataFrame({
+    'dataflowId': save_data,
+    'agencyId': agencyId,
+    'version' : version
+})
 print(df.head())
 
+
+
+
+
+# dataflow.append({'dataflowId': save_data}, ignore_index=True)
+# print(dataflow["dataflowId"].head())
+
+# i need to add agencyID 
+# i need to add version 
