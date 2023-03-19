36447e546d488b60cb030b73e8f4b2b5fb7ec75b
Inderaj <isehmi99@gmail.com>
2023-03-19 11:54:51 +1000

    adding dsd formatting
    dsd formatting which is still in WIP

--- dataflow_picker/pick.py
+++ dataflow_picker/pick.py
@@ -11,6 +11,7 @@ agencyId = []
 version = []
 
 dataflow = pd.DataFrame(columns=['agencyId', 'dataflowId', 'version'])
+dsd = pd.DataFrame()
 
 def open_file(file_name, word):
     with open(file_name, 'r') as file:
@@ -19,15 +20,18 @@ def open_file(file_name, word):
             print("yes")
         else:
             print("no")
+        file.close()
 
 def open_line(file_name, word):
     with open(file_name, 'r'):
         lines = fp.readlines()
         for line in lines:
             if line.find(find)!= -1:
-                # print('Line Number:', lines.index(line))
-                # print('Line:', line)
-                formang_line(line)
+                print('Line Number:', lines.index(line))
+                print('Line:', line)
+                # formang_line(line) 
+        fp.close()
+        lines.close()
 
 
 def formang_line(line):
@@ -53,7 +57,7 @@ def formang_line(line):
 
 
 # open_file(file, find)
-open_line(file, find)
+# open_line(file, find)
 # formang_line(line_test)
 
 # df = pd.DataFrame(save_data, columns=["id"])
@@ -62,7 +66,7 @@ df = pd.DataFrame({
     'agencyId': agencyId,
     'version' : version
 })
-print(df.head())
+# print(df.head())
 
 df.to_csv("test_set")
 
@@ -71,5 +75,38 @@ df.to_csv("test_set")
 # dataflow.append({'dataflowId': save_data}, ignore_index=True)
 # print(dataflow["dataflowId"].head())
 
-# i need to add agencyID 
-# i need to add version 
+
+
+
+# working on dsd now 
+
+
+# the input for this file is 
+#   first we have dataflow 
+#   then we have dsd 
+# the outputs for this file 
+#   we want a pandas data frame for dataflow with dataflowId, agencyId, version
+#   we want a pandas data frame for dsd that has 
+
+
+
+
+# def create_df(file, key_words):
+#     with open(file, "r"):
+#         lines = fp.readlines()
+#         for line in lines:
+#             if line.find(key_words) != -1:
+#                 print('Line Number:', lines.index(line))
+#         lines.close()
+                        
+tb = open("ABS_ALC_children.xml", 'r')
+
+def test(file1):
+    with open(file1, "r"):
+            lines = tb.readlines()
+            for line in lines:
+                print(line)
+
+
+# create_df("ABS_ALC_children.xml", "<Ref id=")
+test("ABS_ALC_children.xml")
