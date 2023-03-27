6188970b17a33e443c1f5ec2ba79df6f96b6972c
Inderaj <isehmi99@gmail.com>
2023-03-27 14:21:38 +1000

    parsing dsd API call into pandas position values
    functions that adds information into the positions and positions_id columns

--- dataflow_picker/pick.py
+++ dataflow_picker/pick.py
@@ -14,7 +14,7 @@ def get_line(file_name, formatLine):
         lines = fp.readlines()
         if formatLine == True:
                     # DataFlow_add_to_df(line, db.parsing_dataflows) 
-                    DSD_add_to_df(line)
+                    DSD_add_to_df(lines)
                 
     fp.close()
     return lines
@@ -29,7 +29,7 @@ def test(lines):
 
 def DataFlow_add_to_df(line, keystring):
 
-    df.loc[df.shape[0]+1, "dataflowId"] =line[line.find(keystring[2][1])+4:line.find(keystring[3][1])-2]
+    df.loc[df.shape[0]+1, "dataflowId"] = line[line.find(keystring[2][1])+4:line.find(keystring[3][1])-2]
 
     df.loc[df.shape[0], "agencyId"] = line[line.find(keystring[4][1])+10:line.find(keystring[5][1])-2]
 
@@ -41,11 +41,56 @@ def DataFlow_add_to_df(line, keystring):
 # print(df)
 
 def DSD_add_to_df(lines):
-    for line in lines:
-        if line.find(find)!= -1:
-            keyword = ['position="','">',]
-            print("helloo")
-            print("we are trying to find the positions", line[line.find(keyword[0]):line.find(keyword[1])])
+    keyword = ['position="','">',"<structure:Enumeration>", ""]
+    positions_counter = 0
+    positions_counter_id = 0
+
+    for i, line in enumerate(lines):
+        if line.find(keyword[0]) != -1: # this is looking for all the lines with positions in it 
+            dsd.loc[dsd.shape[0]+1, "Position"] = line[line.find(keyword[0])+10:line.find(keyword[2])-2]
+            positions_counter = positions_counter + 1
+
+        elif line.find(keyword[2]) != -1: # this is looking for all the lines with the ids 
+            line_temp = lines[i+1]
+            if pd.isna(dsd["Position_id"].loc[dsd.shape[0]]):
+                dsd["Position_id"].loc[dsd.shape[0]] = line_temp[line_temp.find('<Ref id="')+9:line_temp.find(' version')-1]
+            else:
+                old_cell =  dsd["Position_id"].loc[dsd.shape[0]]
+                new_data = line_temp[line_temp.find('<Ref id="')+9:line_temp.find(' version')-1]
+                new_cell = old_cell + "," + new_data
+                dsd["Position_id"].loc[dsd.shape[0]] = new_cell
+    
+    # now we want to fill code and name columns using the infomation we just got 
+    NameCode(lines)
+
+
+    # checking pandas value  
+    print("this is the full data frame")
+    print(dsd.head(10))
+    print("this is just positions ID col")
+    print(dsd["Position_id"].head(10))
+    print("looking at the last element in positions IDS")
+    print(dsd.loc[dsd.shape[0], "Position_id"])
+            # positions_counter_id = positions_counter_id + 1
+            #  check to see if the spot is free (null) if it is save the data if something is there then get the old data and make an list with it 
+
+
+
+
+def NameCode(lines):
+     looking_for = '<structure:Codelist id="' + dsd.loc[1, "Position_id"]
+     print(looking_for)
+     print("hello")
+#  add a for loop here that runs over the dad 
+    #  for i, line in enumerate(lines):
+    #       if line.find() != -1:
+    #            print("hello")
+
+
+
+
+
+
 
 
 
@@ -58,7 +103,12 @@ def DSD_add_to_df(lines):
 # to find positions use this keyword position="
 # to find position id use this keyword <structure:Enumeration> and then get the line under it in that line we want <Ref id="
 
-dsd = pd.DataFrame(columns=["Position", "Position_id", "version"])
+dsd = pd.DataFrame(columns=["Position", "Position_id", "Name", "Code"])
 dsd_file_name = "ABS_ALC_children.xml"
 get_line(dsd_file_name, True)
+
+
+# dsd will have all the information you need to make an api call 
+
+
 
